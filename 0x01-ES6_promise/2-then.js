a88ed9d0a1a2bccb799b8eae6a433898f675eec6
LocalLso <maphalethulocupadadeepmakhele5@gmail.com>
2023-06-28 02:49:34 +0000

    Function handleResponseFromAPI(promise).

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,11 +1,10 @@
-export default function getFullResponseFromAPI(success) {
-  return new Promise((resolve, reject) => {
-    if (success) {
-      resolve({
-        status: 200,
-        body: 'Success',
-      });
-    }
-    reject(new Error('The fake API is not working currently'));
-  });
+// Using prototype function handleResponseFromAPI(promise)
+// Append three handlers to the function
+// .then to hanlde fulfilled, .catch to handle errors, .finally to log message no matter what
+
+export default function handleResponseFromAPI(promise) {
+  return promise
+    .then(() => ({ status: 200, body: 'success' }))
+    .catch(() => new Error())
+    .finally(() => console.log('Got a response from the API'));
 }
